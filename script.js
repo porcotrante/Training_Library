1b552722967617bfd662af8c44ecf09a3ec9f935
Pedro Wagner <pedrowagner0801@gmail.com>
2024-05-21 13:18:28 -0300

    started working on book adding

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ function show() {
 function hide() {
     document.querySelector(".bookForm").style.display = "none";
 }
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
+
+function createBookElement() {
+    const book = document.createElement("div");
+    
+}
+
+function renderBook(arr) {
+    for (i in arr) {
+        document.querySelector(".bookGrid").appendChild(arr[i]);
+    }
+}
+
+const addBookButton = document.querySelector("#addBook");
+const books = [];
+
+addBookButton.addEventListener("click", () => {
+    const book = document.createElement("div");
+    books.push(book);
+    renderBook(books);
+})
